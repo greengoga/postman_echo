3c8e074ebe08b7657fd3b57ee3eed9b59047c1f5
O.Gogol <o.gogol91@gmail.com>
2025-01-12 19:18:10 +0000

    succesful build

--- src/test/java/ru/netology/postmanecho/postmanEchoTest.java
+++ src/test/java/ru/netology/postmanecho/postmanEchoTest.java
@@ -19,6 +19,6 @@ public class postmanEchoTest {
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("wrong data"));
+                .body("data", equalTo("some data"));
     }
 }
